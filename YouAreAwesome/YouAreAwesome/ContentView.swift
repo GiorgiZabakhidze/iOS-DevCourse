6284ad52f27cd131377a7c90c1002281b4500d03
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2023-12-11 02:12:37 +0400

    Awesomeness: Adding a SplashScreen
    Adding a launch animation to the App

--- YouAreAwesome/YouAreAwesome/ContentView.swift
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -16,73 +16,102 @@ struct ContentView: View {
     @State private var soundNumber: Int = -1
     @State private var audioPlayer: AVAudioPlayer!
     
+    @State var animate: Bool = false
+    @State var splashScreen: Bool = true
+    
     var body: some View {
-        VStack {
+        ZStack {
+            VStack {
                 
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(maxWidth: .infinity, maxHeight: 150)
-                .padding()
-            
-            Spacer()
-            
-            Image("image\(imageNumber)")
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(25)
-                .shadow(radius: 30)
-                .padding()
-                
-            Spacer()
-            
-            HStack {
-                Text("Sound is \(soundIsOn ? "On" : "Off")")
-                
-                Toggle("", isOn: $soundIsOn)
-                    .labelsHidden()
-                    .tint(.accentColor)
-                    .onChange(of: soundIsOn, perform: { _ in
-                        if audioPlayer != nil && audioPlayer.isPlaying {
-                            audioPlayer.stop()
-                        }else if audioPlayer != nil {
-                            audioPlayer.play()
-                        }
-                    })
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(maxWidth: .infinity, maxHeight: 150)
+                    .padding()
                 
                 Spacer()
                 
-                Button {
-                    let messages = ["You Are Awesome!",
-                                    "You Are Great!",
-                                    "You Are Fantastic!",
-                                    "Fabulous? That's You!"]
+                Image("image\(imageNumber)")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(25)
+                    .shadow(radius: 30)
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Text("Sound is \(soundIsOn ? "On" : "Off")")
                     
-                    messageString = messages[generateRandomNumber(lastNum: messageNumber, upperBound: 3)]
+                    Toggle("", isOn: $soundIsOn)
+                        .labelsHidden()
+                        .tint(.accentColor)
+                        .onChange(of: soundIsOn, perform: { _ in
+                            if audioPlayer != nil && audioPlayer.isPlaying {
+                                audioPlayer.stop()
+                            }else if audioPlayer != nil {
+                                audioPlayer.play()
+                            }
+                        })
                     
-                    imageNumber = generateRandomNumber(lastNum: imageNumber, upperBound: 9)
+                    Spacer()
                     
-                    soundNumber = generateRandomNumber(lastNum: soundNumber, upperBound: 5)
-                    
-                    if soundIsOn {
-                        playSound(soundName: "sound\(soundNumber)")
-                    }
-                    
-                } label: {
-                    Text("Show Message")
+                    Button {
+                        let messages = ["You Are Awesome!",
+                                        "You Are Great!",
+                                        "You Are Fantastic!",
+                                        "Fabulous? That's You!"]
                         
+                        messageString = messages[generateRandomNumber(lastNum: messageNumber, upperBound: 3)]
+                        
+                        imageNumber = generateRandomNumber(lastNum: imageNumber, upperBound: 9)
+                        
+                        soundNumber = generateRandomNumber(lastNum: soundNumber, upperBound: 5)
+                        
+                        if soundIsOn {
+                            playSound(soundName: "sound\(soundNumber)")
+                        }
+                        
+                    } label: {
+                        Text("Show Message")
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.accentColor)
+                    
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.accentColor)
-
+                .padding()
+                .animation(.smooth)
+                
             }
             .padding()
-
+            
+            ZStack {
+                Color.accentColor
+                
+                Image("launchscreen")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .scaleEffect(animate ? 75 : 1)
+                    .animation(Animation.easeInOut(duration: 0.75))
+            }
+            .ignoresSafeArea(.all)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    self.animate.toggle()
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                    self.splashScreen.toggle()
+                }
+            }
+            .opacity(splashScreen ? 1 : 0)
+            .animation(.default)
         }
-        .padding()
     }
     
     func generateRandomNumber(lastNum: Int, upperBound: Int) -> Int {
