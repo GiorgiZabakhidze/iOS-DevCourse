9d99e16bd4f35a4b71d593e941dbe9f70d76488a
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2023-12-24 01:47:42 +0400

    Logic And Functionality of an App
    Added the way to unlock characters by guessing And implemented random logic so that words won't repeat ever.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var arr = [1, 2, 3, 4]
+
+arr.remove(at: 2)
+
+print(arr)
--- WordGarden/WordGarden/ContentView.swift
+++ WordGarden/WordGarden/ContentView.swift
@@ -10,12 +10,19 @@ import SwiftUI
 struct ContentView: View {
     @State private var wordsGuessed = 0
     @State private var wordsMissed = 0
-    @State private var wordsToGuess = 0
-    @State private var WordsInGame = 0
+    
+    @State private var gameStatusMessage = "How Many Guesses To Uncover The Hidden Word?"
     @State private var flowerNumber = 8
+    
     @State private var characterToGuess = ""
+    @State private var revealedWord = ""
+    @State private var wordToGuess = ""
+    @State private var lettersTried = ""
+    
     @State private var playAgainHidden = true
     @FocusState private var textfieldFocused: Bool
+    
+    @State private var wordsToGuess = ["SWIFT", "DOG", "CAT"]
     
     var body: some View {
         VStack {
@@ -29,16 +36,23 @@ struct ContentView: View {
                 Spacer()
                 
                 VStack(alignment: .leading, content: {
-                    Text("Words to Guess: \(wordsToGuess)")
+                    Text("Words to Guess: \(wordsGuessed)")
                     
-                    Text("Words in Game: \(WordsInGame)")
+                    Text("Words in Game: \(wordsGuessed)")
                 })
                 
             }
             
             Spacer()
             
-            Text("_ _ _ _ _")
+            Text(gameStatusMessage)
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+            
+            Text(revealedWord)
                 .font(.title)
             
             Spacer()
@@ -54,6 +68,13 @@ struct ContentView: View {
                         }
                         .keyboardType(.asciiCapable)
                         .submitLabel(.done)
+                        .onSubmit {
+                            guard characterToGuess != "" else {
+                                return
+                            }
+                            
+                            guessALetter(wordToGuess)
+                        }
                         .autocorrectionDisabled()
                         .textInputAutocapitalization(.characters)
                         .onChange(of: characterToGuess) {
@@ -68,19 +89,34 @@ struct ContentView: View {
                         .focused($textfieldFocused)
                     
                     Button("Guess a Word") {
-                        textfieldFocused = false
-                        //MARK: Guess a word
+                        guessALetter(wordToGuess)
                     }
                     .buttonStyle(.bordered)
                     .tint(.mint)
                     .disabled(characterToGuess.isEmpty)
                     
-                }else {
+                }else if(wordsToGuess.count != 1) {
                     Button("Another Word?") {
-                        //MARK: another word
+                        playAgainHidden = true
+                        lettersTried = ""
+                        wordsToGuess.remove(at: wordsToGuess.firstIndex(of: wordToGuess)!)
+                        wordToGuess = wordsToGuess.randomElement()!
+                        
+                        guessALetter(wordToGuess)
                     }
                     .buttonStyle(.borderedProminent)
                     .tint(.mint)
+                }else {
+                    Text("You Guessed All The Words There Was...")
+                        .font(.largeTitle)
+                        .fontDesign(.serif)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .overlay {
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 10))
+                                .stroke(Color.blue, lineWidth: 2.0)
+                                
+                        }
                 }
             }
 
@@ -92,6 +128,51 @@ struct ContentView: View {
         }
         .padding(.horizontal)
         .ignoresSafeArea(edges: .bottom)
+        .onAppear {
+            wordToGuess = wordsToGuess.randomElement()!
+            
+            revealedWord = "_" + String(repeating: " _", count: wordToGuess.count - 1)
+        }
+        
+    }
+    
+//    func generateRandomWord(_ lastIndex: Int) -> String {
+//        var newIndex: Int
+//        
+//        repeat {
+//            newIndex = Int.random(in: 0..<wordsToGuess.count)
+//        }while newIndex == lastIndex
+//        
+//        return wordsToGuess[newIndex]
+//    }
+    
+    func guessALetter(_ wordToGuess: String) {
+        textfieldFocused = false
+        
+        var lettersGuessed = 0
+        
+        lettersTried += characterToGuess
+        
+        revealedWord = ""
+        
+        for letter in wordToGuess {
+            
+            if lettersTried.contains(letter) {
+                revealedWord += "\(letter) "
+                lettersGuessed += 1
+            }else {
+                revealedWord += "_ "
+            }
+        }
+        
+        revealedWord.removeLast()
+        
+        characterToGuess = ""
+        
+        if(lettersGuessed == wordToGuess.count) {
+            wordsGuessed += 1
+            playAgainHidden = false
+        }
     }
 }
 
